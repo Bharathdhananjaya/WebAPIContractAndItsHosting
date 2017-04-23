33a2a3891f699c6955b03dda450202c99d5313fe
Bharath <bharath82.d@gmail.com>
2017-04-24 00:00:49 +0530

    LIP-0000 : REST API Hosting and Contract Details

--- Framework.Core/CallContext.cs
+++ Framework.Core/CallContext.cs
@@ -8,5 +8,26 @@ namespace Framework.Core
 {
     public class CallContext
     {
+
+        public const string BusinessKey = "business";
+        public const string ChannelKey = "channel";
+        public const string LanguageKey = "language";
+        public const string ProductInstanceKey = "productinstance";
+        public const string RegionKey = "region";
+        public const string RequestIdKey = "requestId";
+
+        private const string region = "PARIS";
+        private string business;
+        private string channel;
+        private string language;
+
+        public string Business { get { return business; } }
+        public string Channel { get { return channel; } }
+
+
+        public string Language { get { return language; } }
+
+        public string Region { get { return region; } protected set { value = region; } }
+       
     }
 }
