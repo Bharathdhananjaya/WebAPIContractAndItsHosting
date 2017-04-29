f919e593cc31c2df3acaa17c5440fefadc09c718
Bharath <bharath82.d@gmail.com>
2017-04-29 19:15:56 +0530

    LIP-0000 : REST API Hosting and Contract Details

--- TestRepository/SQLRepository.cs
+++ TestRepository/SQLRepository.cs
@@ -4,37 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Contracts;
+using Dapper;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace TestRepository
 {
     public class SQLRepository<T> : IRepository<T> where T : class
     {
-        IEnumerable<T> IRepository<T>.GetAll()
+        public SQLRepository()
+        {
+           
+
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString))
+            {
+               return  db.Query<T>("Select * From Employee").ToList();
+            }
+        }
+
+        public T Get(int Id)
+        {
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString))
+            {
+                return db.Query<T>("Select * From Employee WHERE Id = @Id", new { Id }).SingleOrDefault();
+            }
+        }
+
+        public void  Add(T entity)
         {
             throw new NotImplementedException();
         }
 
-        void IRepository<T>.Add(T entity)
+        public void Delete(T entity)
         {
             throw new NotImplementedException();
         }
 
-        void IRepository<T>.Delete(T entity)
+        public void Update(T entity)
         {
             throw new NotImplementedException();
         }
 
-        void IRepository<T>.Update(T entity)
-        {
-            throw new NotImplementedException();
-        }
 
-        T IRepository<T>.Get(int Id)
-        {
-            throw new NotImplementedException();
-        }
-
-        void IDisposable.Dispose()
+        public void Dispose()
         {
             throw new NotImplementedException();
         }
