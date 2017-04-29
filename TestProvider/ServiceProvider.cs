fa546a5ad0d95fc0f62bf00a63952dff24299e18
Bharath <bharath82.d@gmail.com>
2017-04-29 20:31:36 +0530

    LIP-0000 : REST API Hosting and Contract Details

--- TestProvider/ServiceProvider.cs
+++ TestProvider/ServiceProvider.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TestProvider
 {
-    public class ServiceProvider<T> where T : class
+    public class ServiceProvider<T> : IProvider<T> where T: class 
     {
         public List<T> GetListOfItems()
         {
